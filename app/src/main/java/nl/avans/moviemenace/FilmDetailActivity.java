28016f804970c0d7b584857f1c745acecd995241
FSGabrsek <f.gabrsek@gmail.com>
2021-03-26 15:36:57 +0100

    basic Filmdetail page

---
+++ app/src/main/java/nl/avans/moviemenace/FilmDetailActivity.java
@@ -0,0 +1,14 @@
+package nl.avans.moviemenace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FilmDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_film_detail);
+    }
+}
--- app/src/main/java/nl/avans/moviemenace/ui/films/FilmsAdapter.java
+++ app/src/main/java/nl/avans/moviemenace/ui/films/FilmsAdapter.java
@@ -1,5 +1,7 @@
 package nl.avans.moviemenace.ui.films;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,17 +11,28 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import nl.avans.moviemenace.FilmDetailActivity;
 import nl.avans.moviemenace.R;
 
 public class FilmsAdapter extends RecyclerView.Adapter {
-    public static class FilmViewHolder extends RecyclerView.ViewHolder {
+    public static class FilmViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private Context context;
+
         private ImageView mPosterIv;
         private TextView mTitleTv;
 
         public FilmViewHolder(@NonNull View itemView) {
             super(itemView);
+            context = itemView.getContext();
+            itemView.setOnClickListener(this);
+
             mPosterIv = itemView.findViewById(R.id.iv_film_viewholder_poster);
             mTitleTv = itemView.findViewById(R.id.tv_film_viewholder_title);
+        }
+
+        @Override
+        public void onClick(View v) {
+            context.startActivity(new Intent(context, FilmDetailActivity.class));
         }
     }
 
