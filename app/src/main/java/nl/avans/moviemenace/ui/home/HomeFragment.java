37d02a14662c52ce33a8af15a1719d20b3a190a4
FSGabrsek <f.gabrsek@gmail.com>
2021-03-26 16:10:46 +0100

    basic Home page

--- app/src/main/java/nl/avans/moviemenace/ui/home/HomeFragment.java
+++ app/src/main/java/nl/avans/moviemenace/ui/home/HomeFragment.java
@@ -11,25 +11,26 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import nl.avans.moviemenace.R;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private RecyclerView mPopularFilmRecyclerView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        mPopularFilmRecyclerView = root.findViewById(R.id.rv_home_popular_film_list);
+        mPopularFilmRecyclerView.setAdapter(new PopularFilmAdapter());
+        mPopularFilmRecyclerView.setLayoutManager(new LinearLayoutManager(this.getContext(), LinearLayoutManager.HORIZONTAL, false));
+
         return root;
     }
 }
---
+++ app/src/main/java/nl/avans/moviemenace/ui/home/PopularFilmAdapter.java
@@ -0,0 +1,57 @@
+package nl.avans.moviemenace.ui.home;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import nl.avans.moviemenace.FilmDetailActivity;
+import nl.avans.moviemenace.R;
+
+public class PopularFilmAdapter extends RecyclerView.Adapter {
+    public static class PopularFilmViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private Context context;
+
+        private ImageView mPosterIv;
+        private TextView mTitleTv;
+
+        public PopularFilmViewHolder(@NonNull View itemView) {
+            super(itemView);
+            context = itemView.getContext();
+            itemView.setOnClickListener(this);
+
+            mPosterIv = itemView.findViewById(R.id.iv_film_viewholder_poster);
+            mTitleTv = itemView.findViewById(R.id.tv_film_viewholder_title);
+        }
+
+        @Override
+        public void onClick(View v) {
+            context.startActivity(new Intent(context, FilmDetailActivity.class));
+        }
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.film_viewholder, parent, false);
+
+        return new PopularFilmViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+}
