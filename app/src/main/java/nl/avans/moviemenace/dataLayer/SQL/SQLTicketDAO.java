841e6d4b264b23fa5a3409e3f0a0dd7090dac6d3
Bas <bbuijsen@gmail.com>
2021-03-26 16:02:53 +0100

    started on ticketDAO

--- app/src/main/java/nl/avans/moviemenace/dataLayer/SQL/SQLTicketDAO.java
+++ app/src/main/java/nl/avans/moviemenace/dataLayer/SQL/SQLTicketDAO.java
@@ -2,23 +2,51 @@ package nl.avans.moviemenace.dataLayer.SQL;
 
 import java.util.ArrayList;
 
+import nl.avans.moviemenace.dataLayer.DatabaseConnection;
 import nl.avans.moviemenace.dataLayer.IDAO.TicketDAO;
 import nl.avans.moviemenace.domain.Ticket;
 
-public class SQLTicketDAO implements TicketDAO {
+public class SQLTicketDAO extends DatabaseConnection implements TicketDAO {
     @Override
     public void createTicket(Ticket ticket) {
-
+        try{
+            String SQL = "INSERT INTO Ticket (ChairNumber, Email, ViewID, Status, RowNumber) VALUES ("
+                    + ticket.getChairNumber() + ", '" + ticket.getEmail() + "', " + ticket.getViewID() + ", '"
+                    + ticket.getStatus() + "', " + ticket.getRowNumber() + ")";
+            executeSQLStatement(SQL);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
     public Ticket getTicket(int chairnumber, String email, int viewID) {
-        return null;
+        Ticket ticket = null;
+        try{
+            String SQL = "SELECT * FROM Ticket WHERE ChairNumber = " + chairnumber + " AND Email = '" + email + "' AND ViewID = " + viewID;
+            executeSQLSelectStatement(SQL);
+
+            ticket = new Ticket(rs.getInt("ChairNumber"), rs.getString("Email"), rs.getInt("ViewID"), rs.getString("Status"), rs.getInt("RowNumber"));
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return ticket;
     }
 
     @Override
     public ArrayList<Ticket> getAllTicketsForAccount(String email) {
-        return null;
+        ArrayList<Ticket> tickets = new ArrayList<>();
+        try{
+            String SQL = "SELECT * FROM Ticket WHERE Email = '" + email + "'";
+            executeSQLSelectStatement(SQL);
+            while(rs.next()){
+                Ticket ticket = new Ticket(rs.getInt("ChairNumber"), rs.getString("Email"), rs.getInt("ViewID"), rs.getString("Status"), rs.getInt("RowNumber"));
+                tickets.add(ticket);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return tickets;
     }
 
     @Override
