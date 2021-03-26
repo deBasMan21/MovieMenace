7c6be9cc13c21e1e34de77f624e09d7449a62071
QiangLoozen <74371458+QiangLoozen@users.noreply.github.com>
2021-03-26 14:05:39 +0100

    Domein update

--- app/src/main/java/nl/avans/moviemenace/domain/Movie.java
+++ app/src/main/java/nl/avans/moviemenace/domain/Movie.java
@@ -1,4 +1,19 @@
 package nl.avans.moviemenace.domain;
 
+import java.time.LocalDate;
+
 public class Movie {
+    private int id;
+    private String title;
+    private String overview;
+    private LocalDate release_date;
+    private boolean adult;
+
+    public Movie(int id, String title, String overview, LocalDate release_date, boolean adult) {
+        this.id = id;
+        this.title = title;
+        this.overview = overview;
+        this.release_date = release_date;
+        this.adult = adult;
+    }
 }
--- app/src/main/java/nl/avans/moviemenace/domain/Viewing.java
+++ app/src/main/java/nl/avans/moviemenace/domain/Viewing.java
@@ -1,4 +1,49 @@
 package nl.avans.moviemenace.domain;
 
+import java.time.LocalDateTime;
+
 public class Viewing {
+    private int id;
+    private LocalDateTime date;
+    private double price;
+    private boolean threeDimensional;
+
+    public Viewing(int id, LocalDateTime date, double price, boolean threeDimensional) {
+        this.id = id;
+        this.date = date;
+        this.price = price;
+        this.threeDimensional = threeDimensional;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isThreeDimensional() {
+        return threeDimensional;
+    }
+
+    public void setThreeDimensional(boolean threeDimensional) {
+        this.threeDimensional = threeDimensional;
+    }
 }
