152446804ced0878aec7bd79e6c804a66c4dab07
QiangLoozen <74371458+QiangLoozen@users.noreply.github.com>
2021-03-29 11:51:16 +0200

    Account - zipcode validation / format

---
+++ app/src/main/java/nl/avans/moviemenace/domain/ZipCode.java
@@ -0,0 +1,39 @@
+package nl.avans.moviemenace.domain;
+
+public class ZipCode {
+    private String zipCodeValue;
+
+    public ZipCode(String zipCode) {
+        setZipCodeValue(zipCode);
+    }
+
+    public void setZipCodeValue(String zipCode) {
+        String formattedZipCode = formatZipCode(zipCode);
+        if (formattedZipCode == null) {
+            zipCodeValue = null;
+        } else {
+            this.zipCodeValue = formattedZipCode;
+        }
+    }
+
+    public String getZipCodeValue() {
+        return zipCodeValue;
+    }
+
+    private String formatZipCode(String zipCode) {
+
+        String letters = zipCode.trim().substring(4).trim().toUpperCase();
+        int numbersLength = Integer.valueOf(zipCode.trim().substring(0, 4));
+        int lettersLength = zipCode.trim().substring(4).length();
+        char firstLetter = letters.charAt(0);
+        char secondLetter = letters.charAt(1);
+
+        if (numbersLength > 999 && numbersLength <= 9999 && lettersLength == 2 && (firstLetter >= 'A' && firstLetter <= 'Z') && (secondLetter >= 'A' && secondLetter <= 'Z')) {
+            return zipCode.trim().substring(0, 4) + " " + letters;
+        }
+
+        return null;
+    }
+
+
+}
