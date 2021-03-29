1da9dbd697af31e3565bfd4c6d6c1cbb487b77d4
FSGabrsek <f.gabrsek@gmail.com>
2021-03-29 13:57:56 +0200

    added account fields

--- app/src/main/java/nl/avans/moviemenace/EditAccountActivity.java
+++ app/src/main/java/nl/avans/moviemenace/EditAccountActivity.java
@@ -3,6 +3,8 @@ package nl.avans.moviemenace;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class EditAccountActivity extends AppCompatActivity {
@@ -13,9 +15,24 @@ public class EditAccountActivity extends AppCompatActivity {
     private EditText mPostNumEt;
     private EditText mPostCharEt;
 
+    private Button mConfBn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_account);
+
+        mNameEt = findViewById(R.id.et_edit_account_name);
+        mEmailEt = findViewById(R.id.et_edit_account_email);
+        mPlaceEt = findViewById(R.id.et_edit_account_place);
+        mAddressEt = findViewById(R.id.et_edit_account_address);
+        mPostNumEt = findViewById(R.id.et_edit_account_post_num);
+        mPostCharEt = findViewById(R.id.et_edit_account_post_char);
+
+        mConfBn = findViewById(R.id.bn_edit_account_conf);
+
+        mConfBn.setOnClickListener((View v) -> {
+            finish();
+        });
     }
 }
