e234ed2f14fa56ac94d98f64a7dc3293da122064
FSGabrsek <f.gabrsek@gmail.com>
2021-03-30 12:04:00 +0200

    added Floating Action Button to film detail activity

--- app/src/main/java/nl/avans/moviemenace/ui/ListDetailActivity.java
+++ app/src/main/java/nl/avans/moviemenace/ui/ListDetailActivity.java
@@ -5,16 +5,26 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import nl.avans.moviemenace.R;
 
 public class ListDetailActivity extends AppCompatActivity {
     private RecyclerView mListFilmsRv;
 
+    private FloatingActionButton mAddFb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_detail);
+
+        mAddFb = findViewById(R.id.fb_list_detail_add);
+        mAddFb.setOnClickListener((View v) -> {
+
+        });
 
         mListFilmsRv = findViewById(R.id.rv_list_films);
         mListFilmsRv.setAdapter(new ListFilmAdapter());
