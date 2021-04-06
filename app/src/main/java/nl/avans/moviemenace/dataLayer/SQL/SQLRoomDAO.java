8a75cf95a053d8b56b08e7987500c884a3423d4d
Bas <bbuijsen@gmail.com>
2021-04-06 14:00:38 +0200

    added cinema to room object

--- app/src/main/java/nl/avans/moviemenace/dataLayer/SQL/SQLRoomDAO.java
+++ app/src/main/java/nl/avans/moviemenace/dataLayer/SQL/SQLRoomDAO.java
@@ -2,6 +2,7 @@ package nl.avans.moviemenace.dataLayer.SQL;
 
 import nl.avans.moviemenace.dataLayer.DatabaseConnection;
 import nl.avans.moviemenace.dataLayer.IDAO.RoomDAO;
+import nl.avans.moviemenace.domain.Cinema;
 import nl.avans.moviemenace.domain.Room;
 
 public class SQLRoomDAO extends DatabaseConnection implements RoomDAO {
@@ -12,11 +13,13 @@ public class SQLRoomDAO extends DatabaseConnection implements RoomDAO {
         Room room = null;
         try{
             //this string contains the query for selecting the room
-            String SQL = "SELECT * FROM Room WHERE RoomNumber = " + roomNumber;
+            String SQL = "SELECT * FROM Room AS R INNER JOIN Cinema AS C ON C.Name = R.CinemaName WHERE RoomNumber = " + roomNumber;
             //this executes the query
             executeSQLSelectStatement(SQL);
             //this fills in the data for the room
             room = new Room(rs.getInt("RoomNumber"), rs.getInt("NumberOfSeats"), rs.getBoolean("[3D]"), rs.getInt("NumberOfRows"));
+            Cinema cinema = new Cinema(rs.getString("Name"), rs.getString("Place"), rs.getInt("NumberOfRooms"));
+            room.setCinema(cinema);
         } catch (Exception e){
             //Prints out any errors that may occur
             e.printStackTrace();
